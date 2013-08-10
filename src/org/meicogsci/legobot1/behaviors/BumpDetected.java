f99ae2d0a1f929446dacac2148b5e9ece0624e77
DELL <DELL@Inspiron_1764>
2013-08-10 19:29:34 +0200

    BumpDetected

--- src/org/meicogsci/legobot1/behaviors/BumpDetected.java
+++ src/org/meicogsci/legobot1/behaviors/BumpDetected.java
@@ -1,25 +1,36 @@
 package org.meicogsci.legobot1.behaviors;
 
+import org.meicogsci.legobot1.BotSingleton;
+
+import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.TouchSensor;
 import lejos.robotics.subsumption.Behavior;
 
 public class BumpDetected implements Behavior {
-
+	BotSingleton bot = BotSingleton.getInstance();
+	private boolean _suppressed = false;
+    TouchSensor touch = new TouchSensor(SensorPort.S1);
+	
 	@Override
 	public boolean takeControl() {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return touch.isPressed();
 	}
 
 	@Override
 	public void action() {
-		// TODO Auto-generated method stub
-
+		bot.nextAction = "";
+		Motor.B.setSpeed(360);
+        Motor.C.setSpeed(360);
+        Motor.B.rotate(180);
+    	Motor.C.rotate(180);
+		bot.nextAction = "scan";
 	}
 
 	@Override
 	public void suppress() {
-		// TODO Auto-generated method stub
-
+		_suppressed = true;
 	}
 
 }
