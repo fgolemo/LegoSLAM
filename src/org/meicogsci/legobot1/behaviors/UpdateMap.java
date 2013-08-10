38de857d51eea88d5ed6e2333f5e2f8f8e181763
fgolemo <fgolemo@gmail.com>
2013-08-10 18:12:35 +0200

    Added class stubs

---
+++ src/org/meicogsci/legobot1/behaviors/UpdateMap.java
@@ -0,0 +1,25 @@
+package org.meicogsci.legobot1.behaviors;
+
+import lejos.robotics.subsumption.Behavior;
+
+public class UpdateMap implements Behavior {
+
+	@Override
+	public boolean takeControl() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void suppress() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
